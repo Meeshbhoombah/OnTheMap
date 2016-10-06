6951522bd708b63fda965a4337b7d2796a137a0e
Rohan Mishra <rohanmishra2017@gmail.com>
2016-10-06 14:34:14 -0400

    reboot

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -2,135 +2,88 @@
 //  LoginViewController.swift
 //  OnTheMap
 //
-//  Created by benchmark on 9/14/16.
+//  Created by Rohan Mishra on 9/22/16.
 //  Copyright © 2016 Rohan Mishra. All rights reserved.
 //
 
 import UIKit
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
-	
-	@IBOutlet weak var emailField: UITextField!
-	@IBOutlet weak var passwordField: UITextField!
-	@IBOutlet weak var loginButton: UIButton!
-	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		emailField.delegate = self
-		passwordField.delegate = self
-	}
-	
-	override func viewWillAppear(animated: Bool) {
-		super.viewWillAppear(animated)
-		
-		subscribeToKeyboardNotifications()
-	}
-	
-	override func viewWillDisappear(animated: Bool) {
-		super.viewWillDisappear(animated)
-		unsubscribeFromKeyboardNotifications()
-	}
-	
-	@IBAction func login(sender: AnyObject) {
-		func showGenericAlertWithTitle(title: String, message: String) {
-			dispatch_async(dispatch_get_main_queue()){
-				let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
-				let ok = UIAlertAction(title: "OK", style: .Default, handler: nil)
-				alert.addAction(ok)
-				self.presentViewController(alert, animated: true, completion: nil)
-			}
-		}
-		
-		// Do not allow blank entries
-		if (emailField.text! == "" || passwordField.text! == "") {
-			showGenericAlertWithTitle("Login failed", message: "Please enter valid text for both fields")
-			return
-		}
-		
-		activityIndicator.startAnimating()
-		loginButton.enabled = false
-		dismissKeyboard()
-		
-		if Reachability.isConnectedToNetwork() == true {
-			Client.sharedInstance().loginWithUsername(emailField.text!, password: passwordField.text!) { (response, error) in
-				dispatch_async(dispatch_get_main_queue()){
-					self.activityIndicator.stopAnimating()
-					self.loginButton.enabled = true
-				}
-				
-				if error != nil {
-					showGenericAlertWithTitle("Login failed", message: error!.localizedDescription)
-				} else {
-					dispatch_async(dispatch_get_main_queue()) {
-						let controller = self.storyboard!.instantiateViewControllerWithIdentifier("mainTab")
-						self.presentViewController(controller, animated: true, completion: nil)
-					}
-				}
-			}
+class LoginViewController: UIViewController {
 
-		} else {
-			showGenericAlertWithTitle("Connection Error", message: "Check your internet connectivity and try again")
-			return
-		}
-		
-	}
-	
-	@IBAction func signup(sender: AnyObject) {
-		UIApplication.sharedApplication().openURL(NSURL(string:Client.Constants.UdacitySignup)!)
-	}
-	
-	// MARK: Text Field Delegate
-	func textFieldShouldReturn(textField: UITextField) -> Bool {
-		if (textField == emailField){
-			passwordField.becomeFirstResponder()
-		} else if (textField == passwordField){
-			passwordField.resignFirstResponder()
-			login(textField)
-		}
-		
-		return true
-	}
-	
-	// MARK: - Keyboard Management
-	func subscribeToKeyboardNotifications() {
-		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(LoginViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
-		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(LoginViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
-	}
-	
-	func unsubscribeFromKeyboardNotifications() {
-		NSNotificationCenter.defaultCenter().removeObserver(self, name:
-			UIKeyboardWillShowNotification, object: nil)
-		NSNotificationCenter.defaultCenter().removeObserver(self, name:
-			UIKeyboardWillHideNotification, object: nil)
-	}
-	
-	func keyboardWillShow(notification: NSNotification) {
-		let bottom = CGPointMake(0, view.frame.size.height)
-		let lowestPointToShow = CGPointMake(0, loginButton.frame.origin.y + loginButton.frame.size.height)
-		let keyboardTop = CGPointMake(0, bottom.y - getKeyboardHeight(notification))
-		let wiggleRoom = CGFloat(10.0)
-		
-		// Always keep the top of the keyboard a certain distance from the lowest point we want to show, with wiggle room
-		keyboardTop.y < view.convertPoint(lowestPointToShow, toView: view.window).y ? (view.frame.origin.y = (lowestPointToShow.y - keyboardTop.y + wiggleRoom) * (-1)) : (view.frame.origin.y = (keyboardTop.y - lowestPointToShow.y - wiggleRoom))
-	}
-	
-	func keyboardWillHide(notification: NSNotification) {
-		view.frame.origin.y = 0
-	}
-	
-	func getKeyboardHeight(notification: NSNotification) -> CGFloat {
-		let userInfo = notification.userInfo
-		let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
-		return keyboardSize.CGRectValue().height
-	}
-	
-	@IBAction func backgroundTapped(sender: AnyObject) {
-		dismissKeyboard()
-	}
-	
-	func dismissKeyboard() {
-		view.endEditing(true)
-	}
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    let networkRequests: NetworkRequests = NetworkRequests()
+    
+    // MARK:- View Controller Methods
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Fix up the UI
+        enableLogin()
+    }
+    
+    
+    // MARK:- UI Methods
+    
+    @IBAction func login(sender: UIButton) {
+        print("login IBAction called")
+        login()
+    }
+    
+    @IBAction func textFieldUpdated(_ sender: AnyObject) {
+        print("textFieldUpdated called")
+        enableLogin()
+    }
+    
+    
+    // MARK:- Other Methods
+    
+    func enableLogin() {
+        loginButton.isEnabled = !(usernameField.text!.isEmpty || passwordField.text!.isEmpty)
+    }
+    
+    func login() {
+        let username: String = usernameField.text!
+        let password: String = passwordField.text!
+        
+        NSLog("Attemping login with \(username)")
+        
+        // Make the call
+        let loginResult = networkRequests.login(username: username, password: password)
+        
+        // Handle the login outcome
+        switch loginResult {
+        case NetworkRequests.LoginResults.success:
+            NSLog("Login succeeded")
+            performSegue(withIdentifier: "LoggedIn", sender: nil)
+        case NetworkRequests.LoginResults.failedForCredentials:
+            NSLog("Login failed for invalid credentials")
+            showLoginFailureAlert(message: NSLocalizedString("LoginCredentialFailure", comment: "Credentials failure text"))
+        case NetworkRequests.LoginResults.failedForNetworkingError:
+            NSLog("Login failed for networking error")
+            showLoginFailureAlert(message: NSLocalizedString("LoginNetworkFailure", comment: "Network failure text"))
+        }
+    }
+    
+    func showLoginFailureAlert(message: String) {
+        // Construct the alert ingredients
+        let alertController: UIAlertController = UIAlertController(
+            title: NSLocalizedString("LoginFailureTitle", comment: "Login failed alert title"),
+            message: message,
+            preferredStyle:UIAlertControllerStyle.alert)
+        let alertAction: UIAlertAction = UIAlertAction(
+            title: NSLocalizedString("OK", comment: "Not good-OK, not bad-OK, just OK-OK"),
+            style: UIAlertActionStyle.default,
+            handler: nil)
+        
+        // Showtime!
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
+
+    }
+
 }
+
